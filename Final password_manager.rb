b98fe19af8e4c22a32bc284dc4bba8aba1dc38c6
nickwlong <nickwlong@gmail.com>
2022-08-02 11:15:36 +0100

    updated tasks and added test-hash

--- Final password_manager.rb
+++ Final password_manager.rb
@@ -1,5 +1,17 @@
+#Tasks:
+#1/ Make uniquePassword? and uniqueService? (can we include "?" in method names?)
+#2/ Make remove method
+#3/ Make services method
+#4/ Make password_for
+#5/ Later: add / update / sort_by
+
 class PasswordManager2
+
     def initialize
+        @passwords = [
+            {'service' => 'facebook', 'password' => 'qwerty', 'added_on' => '28/7/22'},
+            {'service' => 'amazon', 'password' => 'pwrd123', 'added_on' => '29/7/22'},
+        ]
     end
 
     def uniquePass(pass)
