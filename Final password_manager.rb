5749c3e2ae4ed9e20c1b68f5048a4d2d1928c7c5
nickwlong <nickwlong@gmail.com>
2022-08-02 11:03:31 +0100

    Added file for our final version

---
+++ Final password_manager.rb
@@ -0,0 +1,28 @@
+class PasswordManager2
+    def initialize
+    end
+
+    def uniquePass(pass)
+    end
+
+    def uniqueServ(service)
+    end
+
+    def add
+    end
+
+    def remove
+    end
+
+    def services
+    end
+
+    def sort_by
+    end
+
+    def password_for
+    end
+
+    def update
+    end
+end
