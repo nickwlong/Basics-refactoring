2dd89b1c3455135da1e2c2889984121e3223d80f
nickwlong <nickwlong@gmail.com>
2022-08-03 12:46:55 +0100

    lib and spec folders

---
+++ lib/password_manager_2.rb
@@ -0,0 +1,72 @@
+#Array of objects
+
+class Account
+    def initialize(service, password)
+        @service = service
+        @password = password
+        @time = Time.now
+    end
+
+    attr_accessor :service, :password, :time
+end
+
+
+class PasswordManager2
+
+    def initialize
+        @manager = []
+    end
+
+    def uniquePass(pass)
+        !@manager.any?{|account| account.password == pass}
+    end
+
+    def uniqueServ(serv)
+        !@manager.any?{|account| account.service == serv}
+    end
+
+    def add(service, password)
+        @manager << Account.new(service, password) if uniquePass(password) && uniqueServ(service) 
+    end
+
+    def remove(service)
+        @manager.delete_if{|account| account.service == service}
+    end
+
+    def services
+        @manager.map{|account| account.service}
+    end
+
+    def sort_by(type, order = nil)
+
+        if type == 'service'
+            @manager.sort_by!{|account| account.service}
+        elsif type == 'added_on'
+            @manager.sort_by!{|account| account.time}
+        end
+
+        @manager.reverse! if order == 'reverse'
+
+        @manager.map{|account| account.service}
+    end
+
+    def password_for(service)
+        @manager.find{|account| account.service == service}.password
+    end
+
+    def update(service, new_password)
+
+        account = @manager.find{|account| account.service == service}
+
+        if uniquePass(new_password)
+            account.password = new_password
+            account.time = Time.now
+        end
+    end    
+end
+
+
+
+
+
+
